0b68e3cec7e67eb4d0bb6fed7daa201fefb12810
Anton Kolotaev <anton.kolotaev@gmail.com>
2012-02-24 16:15:28 +0000

    computation timeout set to 30 sec

--- src/templates/karrigell/run_computation.py
+++ src/templates/karrigell/run_computation.py
@@ -1,5 +1,7 @@
 import traceback
 
+computation_timeout = 30
+
 if g_errors <> []:
     run_computation = False
 
@@ -47,15 +49,14 @@ if run_computation and len(REQUEST) > n_elem(['m','f','o','meth',"Model_Size"]):
             res_t = []
             process = Process(target = G, args = (opt,model,queue,iterate_object,iterate_name,iterate_to,int(_iteration_steps), iteration_getter, iteration_setter))
         process.start()
-        threshold = 10
         try:
             if iterate_object == None:
-                res.extend(queue.get(timeout=threshold))
+                res.extend(queue.get(timeout=computation_timeout))
             else:
                 begin = time()
                 iterations = int(_iteration_steps)
                 for i in range(iterations):
-                    q = queue.get(timeout=begin + threshold - time())
+                    q = queue.get(timeout=begin + computation_timout - time())
                     res.extend(q)
                     if res_t == []:
                         for k,v in q:
@@ -71,9 +72,9 @@ if run_computation and len(REQUEST) > n_elem(['m','f','o','meth',"Model_Size"]):
             if res == []: res = None
             if res_t == []: res_t = None
             process.terminate()
-            raise Exception("Method has worked more than " + str(threshold) + "s. Please try another parameter combination")
+            raise Exception("Method has worked more than " + str(computation_timeout) + "s. Please try another parameter combination")
             
-        process.join(timeout=threshold)
+        process.join(timeout=computation_timeout)
     except Exception, exc:
         #print "exception caught:"
         #print traceback.format_exc()
