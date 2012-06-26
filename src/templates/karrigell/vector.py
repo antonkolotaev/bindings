bd3b256f25faea07fb980db1fdaf03c516f84317
Anton Kolotaev <anton.kolotaev@gmail.com>
2012-06-26 14:59:29 +0000

    vector.py as  module

--- src/templates/karrigell/vector.py
+++ src/templates/karrigell/vector.py
@@ -1,17 +1,24 @@
-def renderVector(table, colors, label, vlabel, pmem):
-   def mc(vlabel, label):
-      if not history_mode:
-         return INPUT(name=vlabel,value=label)
+from HTMLTags import *
+
+def render(v, table, colors, fullName, friendlyName, pmem):
+   def mc(fullName, friendlyName):
+      if not v.ctx.history_mode:
+         return INPUT(name=fullName,value=friendlyName)
       else:
-         return label
+         return friendlyName
    def rc(s):
-      if not history_mode:
+      if not v.ctx.history_mode:
          return s  
       else:
          return ''    
-   clrinc()
-   table <= TR(TD(label, align='right',rowspan=len(pmem)) + TD(mc(vlabel + '[0]',pmem[0])) + TD(rc('R'),rowspan=len(pmem)),bgcolor=clr(colors,clridx))
-   table <= Sum([TR(TD(mc(vlabel + '[' + str(i) + ']',pmem[i])),bgcolor=clr(colors,clridx)) for i in range(1,len(pmem))])
+   v.clrinc()
+   table <= TR(TD(friendlyName, align='right',rowspan=len(pmem)) + 
+               TD(mc(fullName + '[0]',pmem[0])) + 
+               TD(rc('R'),
+               rowspan=len(pmem)),
+               bgcolor=v.currentColor)
+               
+   table <= Sum([TR(TD(mc(fullName + '[' + str(i) + ']',pmem[i])),bgcolor=v.currentColor) for i in range(1,len(pmem))])
 
 def iterVector(ctx, pmem, label, vlabel):
    
@@ -23,21 +30,16 @@ def iterVector(ctx, pmem, label, vlabel):
    ctx._iterables_setter.extend(map(lambda i: (lambda z: pmem.__setitem__(i, z)), range(len(pmem))))
    
 
-def loadVector(property_name, vlabel, pmem):
+def load(ctx, property_name, vlabel, pmem):
     try:
        for i in range(len(pmem)):
           src = vlabel + "[" + str(i) + "]"
-          if src in REQUEST: 
-             pmem[i] = float(REQUEST[src])
+          if src in ctx.REQUEST: 
+             pmem[i] = float(ctx.REQUEST[src])
     except Exception, ex:
-       add_error('Error in' + property_name + ':' + str(ex))
+       ctx.addError('Error in' + property_name + ':' + str(ex))
 
-def processVector(ctx, table, colors, obj, propname, label, vlabel):
+def getIterables(ctx, table, colors, obj, propname, label, vlabel):
    pmem = getattr(obj, propname)
-   #if ctx.reload and not history_mode:
-   #   loadVector(propname, vlabel, pmem)
-   #renderVector(table, colors, label, vlabel, pmem)
    iterVector(ctx, pmem, label, vlabel)
-
-
 
